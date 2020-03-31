0005bb701e305615222daf0fe9291ce6e2b49a2e
abrych <arkadiusz.brych@external.t-mobile.pl>
2020-03-31 12:27:59 +0200

    Author: Arkadiusz Brych Description: JWT Initial commit

---
+++ src/main/java/pl/brych/jwtapi/filter/JwtFilter.java
@@ -0,0 +1,52 @@
+package pl.brych.jwtapi.filter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+
+    public static final String pass = "A?D(G+KbPdSgVkYp3s6v9y$B&E)H@McQfThWmZq4t7w!z%C*F-JaNdRgUkXn2r5u";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String authorization = httpServletRequest.getHeader("Authorization");
+
+        UsernamePasswordAuthenticationToken authenticationToken = getUsernamePasswordAuthenticationToken(authorization);
+
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    private UsernamePasswordAuthenticationToken getUsernamePasswordAuthenticationToken(String authorization) {
+        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC512(pass)).build();
+        DecodedJWT verify = jwtVerifier.verify(authorization.substring(7));
+        String name = verify.getClaim("name").asString();
+        Boolean isAdmin = verify.getClaim("admin").asBoolean();
+        String role = "ROLE_USER";
+        if (isAdmin)
+            role = "ROLE_ADMIN";
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(role);
+        return new UsernamePasswordAuthenticationToken(name,
+                null,
+                Collections.singleton(simpleGrantedAuthority));
+    }
+}
+
